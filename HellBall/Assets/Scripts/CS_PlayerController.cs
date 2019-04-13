6ada60a1b84040bd87ace4f50fc6c1cc7230d3bc
batherit <batherit0703@naver.com>
2019-04-13 17:36:00 +0900

    [ADD]  Player movement

---
+++ HellBall/Assets/Scripts/CS_PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CS_PlayerController : MonoBehaviour {
+
+    private float dirX, dirY;
+    CS_UIButtonManager UIButtonManager;
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        UIButtonManager = GameObject.Find("Managers").GetComponent<CS_UIButtonManager>();
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        dirX = 0.0f;
+        dirY = 0.0f;
+        if(UIButtonManager.IsLeftDown()) { dirX -= 1.0f; }
+        if(UIButtonManager.IsRightDown()) { dirX += 1.0f; }
+        if(UIButtonManager.IsJumpDown()) { dirY += 1.0f; }
+        if(UIButtonManager.IsDiveDown()) { dirY -= 1.0f; }
+
+        rb.AddForce(new Vector2(dirX * Time.deltaTime * 200.0f, dirY * Time.deltaTime * 500.0f));
+	}
+}
---
+++ HellBall/Assets/Scripts/CS_UIButtonManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_UIButtonManager : MonoBehaviour {
+
+    private bool isLeftDown;
+    private bool isRightDown;
+    private bool isJumpDown;
+    private bool isDiveDown;
+
+    private void Start()
+    {
+        isLeftDown = false;
+        isRightDown = false;
+        isJumpDown = false;
+        isDiveDown = false;
+    }
+
+    public void LeftDown() { isLeftDown = true; }
+    public void LeftUp() { isLeftDown = false; }
+    public bool IsLeftDown() { return isLeftDown; }
+
+    public void RightDown() { isRightDown = true; }
+    public void RightUp() { isRightDown = false; }
+    public bool IsRightDown() { return isRightDown; }
+
+    public void JumpDown() { isJumpDown = true; }
+    public void JumpUp() { isJumpDown = false; }
+    public bool IsJumpDown() { return isJumpDown; }
+
+    public void DiveDown() { isDiveDown = true; }
+    public void DiveUp() { isDiveDown = false; }
+    public bool IsDiveDown() { return isDiveDown; }
+}
