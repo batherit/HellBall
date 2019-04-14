8fde08462bcbfb847261708a9af8d82e1c4a7626
batherit <batherit0703@naver.com>
2019-04-15 01:50:35 +0900

    [ADD] Obstacles

---
+++ HellBall/Assets/Scripts/CS_ObstacleSet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_ObstacleSet : MonoBehaviour {
+
+    public GameObject ObstacleLeft;
+    public GameObject ObstacleRight;
+
+    // Use this for initialization
+    void Start()
+    {
+        // this의 position y값은 ObstacleGenerator가 지정한다.
+        ObstacleLeft.transform.localPosition = new Vector3(Random.Range(-7.85f, -6.25f), 0.0f, 0.0f);
+        ObstacleRight.transform.localPosition = new Vector3(Random.Range(6.25f, 7.85f), 0.0f, 0.0f);
+    }
+}
---
+++ HellBall/Assets/Scripts/CS_ObstacleSub.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_ObstacleSub : MonoBehaviour {
+
+    public GameObject thomUp;
+    public GameObject thomDown;
+
+    // Use this for initialization
+    void Start () {
+        thomUp.transform.localPosition = new Vector3(Random.Range(-4.9f, 4.9f), 7.0f, 0.0f);
+        thomDown.transform.localPosition = new Vector3(Random.Range(-4.9f, 4.9f), -7.0f, 0.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
