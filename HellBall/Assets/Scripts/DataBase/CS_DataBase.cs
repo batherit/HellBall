dd979665f0e7d3ee291e040733ab92cfdb2b0664
batherit <batherit0703@naver.com>
2019-05-06 00:20:43 +0900

    [ADD] Database about guns.

---
+++ HellBall/Assets/Scripts/DataBase/CS_DataBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_DataBase : MonoBehaviour {
+
+    private static CS_DataBase instance;
+
+    private Hashtable guns = new Hashtable();
+
+    private void Start()
+    {
+        guns.Add("Gun", new S_GunInfo(E_ShotMode.Independent, 100.0f, 0.2f, 0.25f, 0.0f, 6, 0.8f));
+    }
+
+    public static CS_DataBase Instance
+    {
+        get
+        {
+            if(!instance)
+            {
+                instance = FindObjectOfType<CS_DataBase>();
+            }
+
+            return instance;
+        }
+    }
+
+    public S_GunInfo GetGunInfo(string gunName)
+    {
+        return guns[gunName] as S_GunInfo;
+    }
+}
---
+++ HellBall/Assets/Scripts/Structures/S_GunInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum E_ShotMode { Independent, Group };   // 독립, 집단
+                                                 // 독립 : 단발, 연사, 미사일, 레이져, 다연발
+                                                 // 집단 : 점사, 샷건
+
+[System.Serializable]
+public class S_GunInfo {
+    public E_ShotMode shotMode;
+    public float effectiveRange;       // 유효 사정 거리
+    public float reboundDegree;        // 반동 정도(총이 뒤로 밀리는 정도)
+    public float delay;                // 다음 발사까지 딜레이
+    public float delayInGroupMode;     // 그룹 모드에서의 각 탄환의 발사 시간 간격
+    public int maxBulletNum;           // 최대 탄환 수 (불변)
+    public float reloadTime;           // 재장전 시간
+
+    public S_GunInfo(E_ShotMode _shotMode, float _effectiveRange, float _reboundDegree, float _delay, float _delayInGroupMode, int _maxBulletNum, float _reloadTime)
+    {
+        effectiveRange = _effectiveRange;
+        shotMode = _shotMode;
+        reboundDegree = _reboundDegree;
+        delay = _delay;
+        delayInGroupMode = _delayInGroupMode;
+        maxBulletNum = _maxBulletNum;
+        reloadTime = _reloadTime;
+    }
+}
